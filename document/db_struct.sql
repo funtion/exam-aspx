956f2f3d797708a0c38101b3c386b9b65236bfc8
funtion <xieshufang76@gmail.com>
2014-09-25 21:40:49 +0800

    add model base and student model

---
+++ document/db_struct.sql
@@ -0,0 +1,116 @@
+delimiter $$
+
+CREATE DATABASE `exam` /*!40100 DEFAULT CHARACTER SET utf8 */$$
+
+
+delimiter $$
+
+CREATE TABLE `announcement` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(1024) DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+
+delimiter $$
+
+CREATE TABLE `examiation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `time` int(11) DEFAULT NULL,
+  `sNumber` int(11) DEFAULT NULL,
+  `mNumber` int(11) DEFAULT NULL,
+  `tNumber` int(11) DEFAULT NULL,
+  `sQuestion` varchar(1024) DEFAULT NULL,
+  `tQuestion` varchar(1024) DEFAULT NULL,
+  `mQuestion` varchar(1024) DEFAULT NULL,
+  `ready` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+
+delimiter $$
+
+CREATE TABLE `file` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `path` varchar(45) DEFAULT NULL,
+  `time` time DEFAULT NULL,
+  `size` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+
+delimiter $$
+
+CREATE TABLE `limituser` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sid` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+
+delimiter $$
+
+CREATE TABLE `option` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(1024) DEFAULT NULL,
+  `picture` varchar(1024) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+
+delimiter $$
+
+CREATE TABLE `question` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` int(11) NOT NULL,
+  `answer` varchar(1024) DEFAULT NULL,
+  `options` varchar(45) DEFAULT NULL,
+  `picture` varchar(45) DEFAULT NULL,
+  `discription` varchar(45) DEFAULT NULL,
+  `examinationId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+
+delimiter $$
+
+CREATE TABLE `result` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `studentId` int(11) DEFAULT NULL,
+  `examinationId` int(11) DEFAULT NULL,
+  `sQuestion` varchar(1024) DEFAULT NULL,
+  `mQuestion` varchar(1024) DEFAULT NULL,
+  `tQuestion` varchar(1024) DEFAULT NULL,
+  `sScore` float DEFAULT NULL,
+  `mSCore` float DEFAULT NULL,
+  `tScore` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+
+delimiter $$
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sid` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `sid_UNIQUE` (`sid`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+
+delimiter $$
+
+CREATE TABLE `teacher` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_UNIQUE` (`user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+
