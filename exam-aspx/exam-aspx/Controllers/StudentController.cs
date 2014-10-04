aefe76bbd6c41ee70047eb5bb9ff04bca2b9762e
funtion <xieshufang76@gmail.com>
2014-10-04 22:34:29 +0800

    add announce page

--- exam-aspx/exam-aspx/Controllers/StudentController.cs
+++ exam-aspx/exam-aspx/Controllers/StudentController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using exam_aspx.Models;
+using exam_aspx.Entity;
+
 
 namespace exam_aspx.Controllers
 {
@@ -16,13 +19,16 @@ namespace exam_aspx.Controllers
         }
 
         [HttpGet]
-        public ActionResult Index()
+        public ActionResult Index()//公告
         {
             int sid = getSid();
             if (sid == -1)
             {
                 return Redirect("/Index/Index");
             }
+            AnnouncementModel model = new AnnouncementModel();
+            var data = model.getAnnouncements();
+            ViewData["announcement"]=data;
             return View();
         }
         [HttpGet]
