dca7d5acabb21c650fbbf9cd8c6d1611c81e9743
funtion <xieshufang76@gmail.com>
2014-12-09 13:06:22 +0800

    re fact

--- exam-aspx/exam-aspx/Controllers/ProjectController.cs
+++ exam-aspx/exam-aspx/Controllers/ProjectController.cs
@@ -11,29 +11,33 @@ namespace exam_aspx.Controllers
 {
     public class ProjectController : Controller
     {
+        const string COURSE = "course";
+        const string YEAR = "year";
+        const string HOMEWORK = "homework";
+        const string STUDENT = "student";
         //
         // GET: /Project/
         [HttpGet]
         public ActionResult Index()
         {
-
+            
             var model = new ProjectModel();
-            if (Request.Params["course"] != "")
+            if (Request.Params[COURSE] != "")
             {
-                if (Request.Params["year"] != "")
+                if (Request.Params[YEAR] != "")
                 {
-                    if (Request.Params["homework"] != "")
+                    if (Request.Params[HOMEWORK] != "")
                     {
-
+                        ViewBag.data = model.getAllProjectStudent(Request.Params[COURSE], Request.Params[YEAR], Request.Params[HOMEWORK]);
                     }
                     else
                     {
-                        ViewBag.data = model.getAllHomeWork(Request.Params["course"], Request.Params["year"]);
+                        ViewBag.data = model.getAllHomeWork(Request.Params[COURSE], Request.Params[YEAR]);
                     }
                 }
                 else
                 {
-                    ViewBag.data = model.getAllYears(Request.Params["course"]);
+                    ViewBag.data = model.getAllYears(Request.Params[COURSE]);
                 }
             }
             else
@@ -47,14 +51,12 @@ namespace exam_aspx.Controllers
         public ActionResult Info()
         {
             var model = new ProjectModel();
-            string course = Request.Params["course"];
-            string year = Request.Params["year"];
-            string homeork = Request.Params["homework"];
-            string student = Request.Params["student"];
+            string course = Request.Params[COURSE];
+            string year = Request.Params[YEAR];
+            string homeork = Request.Params[HOMEWORK];
+            string student = Request.Params[STUDENT];
             ViewBag.data = model.getAllProject(course, year, homeork, student);
             return View();
         }
-
-
     }
 }
